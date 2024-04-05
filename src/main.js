ad6ab8371019bd9b516c028e19b170440ec7c93c
tediko <tediko123@wp.pl>
2024-04-05 22:33:54 +0200

    Added image swipe

--- src/main.js
+++ src/main.js
@@ -97,3 +97,30 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev"
       }
   });
+
+// ----------------
+// IMAGE SWIPE
+// ----------------
+const overlays = document.querySelectorAll('[data-kv-overlay]');
+const imageElement1 = document.querySelector('[data-kv-image1]');
+const imageElement2 = document.querySelector('[data-kv-image2]');
+const imagesSrc = [imageElement1.src, imageElement2.src]
+
+function change() {
+    let randomNumber = Math.round(Math.random());
+    overlays.forEach(overlay => {
+        overlay.classList.add('active');
+
+        overlay.addEventListener('transitionend', function transition() {
+            imageElement1.src = imagesSrc[randomNumber == 1 ? 0 : 1];
+            imageElement2.src = imagesSrc[randomNumber == 1 ? 1 : 0];
+            overlay.classList.remove('active');
+
+            overlay.removeEventListener('transitionend', transition);
+        })
+    })
+}
+
+window.onload = function () {
+    setInterval(change, 4000);
+};
