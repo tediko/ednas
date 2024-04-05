0a48a44a95fc11f05d674764ec452c5015ffb11e
tediko <tediko123@wp.pl>
2024-04-05 11:30:10 +0200

    Added nav on scroll and menu navigation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,55 @@
 import './sass/main.scss';
+
+// ----------------
+// MENU NAVIGATION
+// ----------------
+const menuButton = document.querySelector('[data-menu]');
+let isOpen = false;
+let isClosing = false;
+let menuTriggerDelay = 500;
+
+// Adding some delay to prevent user from toggling menu instantly.
+const setTriggerDelay = () => {
+    isClosing = true;
+
+    setTimeout(() => {
+        isClosing = false;
+    }, menuTriggerDelay);
+}
+
+// Toggle menu
+const toggleMenu = () => {
+    if (isClosing) return;
+    isOpen = !isOpen;
+    setTriggerDelay()
+        
+    if (isOpen) {
+        menuButton.setAttribute("aria-expanded", isOpen);
+    }
+    
+    if (!isOpen) {
+        menuButton.setAttribute("aria-expanded", isOpen);
+    }
+}
+
+// Event listeners
+menuButton.addEventListener('click', () => toggleMenu());
+
+// ----------------
+// NAV ON SCROLL
+// ----------------
+const navElement = document.querySelector('[data-nav]');
+
+const hideNavOnScroll = () => {
+    let offsetScrollY = window.scrollY;
+    let triggerOffset = 20;
+
+    if (offsetScrollY > triggerOffset) {
+        navElement.classList.add('on-scroll')
+    } else {
+        navElement.classList.remove('on-scroll')
+    }
+}
+
+// Event listeners
+document.addEventListener('scroll', hideNavOnScroll)
