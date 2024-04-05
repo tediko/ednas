411e76ea27750719159c5fac0e0d4ce334292171
tediko <tediko123@wp.pl>
2024-04-06 00:57:11 +0200

    Added different image swipe for desktop and mobile

--- src/swipeImages.js
+++ src/swipeImages.js
@@ -1,16 +1,25 @@
 const overlays = document.querySelectorAll('[data-kv-overlay]');
 const imageElement1 = document.querySelector('[data-kv-image1]');
 const imageElement2 = document.querySelector('[data-kv-image2]');
+const imageElement3 = document.querySelector('[data-kv-image3]');
+let windowSize = window.innerWidth;
+let imagesSrcFirst = [imageElement1.src, imageElement2.src, imageElement3.src]
 
 function change() {
-    let imagesSrc = [imageElement1.src, imageElement2.src]
+    let imagesSrc = [imageElement1.src, imageElement2.src, imageElement3.src]
 
     overlays.forEach(overlay => {
         overlay.classList.add('active');
 
         overlay.addEventListener('transitionend', function transition() {
-            imageElement1.src = imagesSrc[1];
-            imageElement2.src = imagesSrc[0];
+            if (windowSize < 769) {
+                imageElement1.src = imagesSrc[2];
+                imageElement3.src = imagesSrc[0];
+            } else {
+                imageElement1.src = imagesSrc[1];
+                imageElement2.src = imagesSrc[2];
+                imageElement3.src = imagesSrc[0];
+            }
             overlay.classList.remove('active');
 
             overlay.removeEventListener('transitionend', transition);
@@ -18,6 +27,16 @@ function change() {
     })
 }
 
+window.addEventListener('resize', () => {
+    windowSize = window.innerWidth;
+
+    if (windowSize < 769) {
+        imageElement1.src = imagesSrcFirst[0];
+        imageElement2.src = imagesSrcFirst[1];
+        imageElement3.src = imagesSrcFirst[2];
+    }
+})
+
 export default window.onload = function () {
-    setInterval(change, 4000);
+    setInterval(change, 2000);
 };
