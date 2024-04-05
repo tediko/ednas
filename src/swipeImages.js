ab6f460dc8620a19a9b602aa1c46470e42f58cbc
tediko <tediko123@wp.pl>
2024-04-06 00:04:38 +0200

    Removed randomNumber

--- src/swipeImages.js
+++ src/swipeImages.js
@@ -1,17 +1,16 @@
 const overlays = document.querySelectorAll('[data-kv-overlay]');
 const imageElement1 = document.querySelector('[data-kv-image1]');
 const imageElement2 = document.querySelector('[data-kv-image2]');
-const imagesSrc = [imageElement1.src, imageElement2.src]
 
 function change() {
-    let randomNumber = Math.round(Math.random());
-    
+    let imagesSrc = [imageElement1.src, imageElement2.src]
+
     overlays.forEach(overlay => {
         overlay.classList.add('active');
 
         overlay.addEventListener('transitionend', function transition() {
-            imageElement1.src = imagesSrc[randomNumber == 1 ? 0 : 1];
-            imageElement2.src = imagesSrc[randomNumber == 1 ? 1 : 0];
+            imageElement1.src = imagesSrc[1];
+            imageElement2.src = imagesSrc[0];
             overlay.classList.remove('active');
 
             overlay.removeEventListener('transitionend', transition);
