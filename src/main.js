c8d08aca099fb299069709c8bb3933619120547f
tediko <tediko123@wp.pl>
2024-04-05 21:44:54 +0200

    Added Navigation modules

--- src/main.js
+++ src/main.js
@@ -84,8 +84,10 @@ slides.forEach(slide => {
 // SWIPER
 // ----------------
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
 const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
     loop: false,
     slidesPerView: "auto",
     spaceBetween: 20,
